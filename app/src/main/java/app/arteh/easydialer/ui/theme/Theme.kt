241a01f8b47909a4daabf937809e2d54d037d10f
ali <ali_teimoury1@yahoo.com>
2026-02-08 07:11:13 -0600

    idk

--- app/src/main/java/app/arteh/easydialer/ui/theme/Theme.kt
+++ app/src/main/java/app/arteh/easydialer/ui/theme/Theme.kt
@@ -1,17 +1,13 @@
 package app.arteh.easydialer.ui.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.staticCompositionLocalOf
-import androidx.compose.ui.platform.LocalContext
 
 private val LocalAppTypography = staticCompositionLocalOf<AppTypography> {
     error("No AppTypography provided")
