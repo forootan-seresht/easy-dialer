3691c29396a6585d6124ba2163bdf95f46067521
ali <ali_teimoury1@yahoo.com>
2026-02-08 07:25:37 -0600

    added image for call logs

--- app/src/main/java/app/arteh/easydialer/clog/CLogUI.kt
+++ app/src/main/java/app/arteh/easydialer/clog/CLogUI.kt
@@ -1,24 +1,44 @@
 package app.arteh.easydialer.clog
 
+import android.provider.MediaStore
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import app.arteh.easydialer.Holder
 import app.arteh.easydialer.R
 import app.arteh.easydialer.clog.models.Clog
+import app.arteh.easydialer.ui.theme.AppColor
+import app.arteh.easydialer.ui.theme.appTypography
 
 @Composable
 fun CLogScreen(cLogVM: CLogVM) {
@@ -36,6 +56,29 @@ fun CLogScreen(cLogVM: CLogVM) {
 
 @Composable
 private fun ItemCallLog(idx: Int, log: Clog) {
+    val context = LocalContext.current
+    var bitmap by remember(log) { mutableStateOf<ImageBitmap?>(null) }
+
+    val firstChar = log.contact?.name?.firstOrNull()?.uppercaseChar() ?: '#'
+    val color = if (log.contact != null)
+        Holder.colors[(firstChar).toInt() % 7]
+    else AppColor.Gray1.resolve()
+
+    if (log.contact != null) {
+        LaunchedEffect(log) {
+            try {
+                bitmap =
+                    log.contact.thumbUri?.let {
+                        MediaStore.Images.Media.getBitmap(
+                            context.contentResolver,
+                            it
+                        ).asImageBitmap()
+                    }
+            } catch (e: Exception) {
+            }
+        }
+    }
+
     Row(
         Modifier
             .padding(horizontal = 10.dp, vertical = 5.dp)
@@ -44,15 +87,49 @@ private fun ItemCallLog(idx: Int, log: Clog) {
                 color = MaterialTheme.colorScheme.surface,
                 RoundedCornerShape(5.dp)
             )
-            .padding(10.dp)
+            .padding(10.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Column(Modifier.weight(1f)) {
-            if (log.contact == null)
-                Text(text = "Unknown")
-            else
-                Text(text = log.contact.name, fontWeight = FontWeight.Bold)
+        if (bitmap != null)
+            Image(
+                modifier = Modifier
+                    .size(60.dp)
+                    .clip(CircleShape),
+                bitmap = bitmap!!,
+                contentDescription = null
+            )
+        else
+            Box(
+                modifier = Modifier
+                    .size(60.dp)
+                    .background(color, CircleShape),
+                contentAlignment = Alignment.Center
+            ) {
 
-            Text(text = log.number)
+                if (log.contact != null)
+                    Text(
+                        text = firstChar.toString(),
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White,
+                        fontSize = 18.sp
+                    )
+                else Icon(
+                    modifier = Modifier.size(40.dp),
+                    painter = painterResource(R.drawable.person),
+                    contentDescription = null,
+                    tint = Color.Black,
+                )
+            }
+
+        Column(Modifier
+            .padding(horizontal = 10.dp)
+            .weight(1f)) {
+            if (log.contact == null)
+                Text(text = log.number, fontWeight = FontWeight.Bold)
+            else {
+                Text(text = log.contact.name, fontWeight = FontWeight.Bold)
+                Text(text = log.number, style = MaterialTheme.appTypography.desc)
+            }
         }
 
         val pair = when (log.status) {
