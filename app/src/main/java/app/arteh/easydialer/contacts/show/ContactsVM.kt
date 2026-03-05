c92820e0af4cf5dc67a66ad19e38b521b2af43d4
ali <ali_teimoury1@yahoo.com>
2026-03-05 02:12:49 -0500

    added search contact by name added go add contact

--- app/src/main/java/app/arteh/easydialer/contacts/show/ContactsVM.kt
+++ app/src/main/java/app/arteh/easydialer/contacts/show/ContactsVM.kt
@@ -1,11 +1,14 @@
 package app.arteh.easydialer.contacts.show
 
 import android.app.Application
+import android.content.Context
+import android.content.Intent
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import app.arteh.easydialer.contacts.ContactRP
+import app.arteh.easydialer.contacts.edit.EditContactActivity
 import app.arteh.easydialer.contacts.show.models.Contact
 import app.arteh.easydialer.contacts.show.models.UIState
 import kotlinx.coroutines.Dispatchers
@@ -28,19 +31,32 @@ class ContactsVM(application: Application, val contactRP: ContactRP) :
     fun load() {
         if (!loaded) {
             loaded = true
-            loadContacts()
+            reloadContacts()
         }
     }
 
-    fun loadContacts() {
+    fun reloadContacts() = searchContact(uiState.value.searchText)
+
+    private fun searchContact(name: String) {
         viewModelScope.launch(Dispatchers.IO) {
-            val map = contactRP.loadContacts()
+            val map = contactRP.loadContacts(name)
             _items.emit(map)
         }
     }
 
     fun updateSearchText(text: String) {
         _uiState.update { it.copy(searchText = text) }
+        searchContact(text)
+    }
+
+    fun goAddContact(context: Context) {
+        val intent = Intent(context, EditContactActivity::class.java)
+        intent.apply {
+            putExtra("id", 0L)
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        }
+
+        context.startActivity(intent)
     }
 
     class Factory(
